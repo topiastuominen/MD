d4ed98eda9eda6a9d98d6e57415648b80710d632
topiastuominen <128483989+topiastuominen@users.noreply.github.com>
2025-04-01 13:19:57 +0300

    Add files via upload

---
+++ MightyDucks/tapahtumat.js
@@ -0,0 +1,36 @@
+
+                  document.getElementById('submit-button').addEventListener('click', function(event) {
+                    event.preventDefault(); // Prevent form submission
+                    const selectedEvent = document.getElementById('goal-en').value;
+                    const somethingElseInput = document.getElementById('something-else-input');
+
+                    const answers = {
+                      selectedEvent: selectedEvent,
+                      somethingElse: somethingElseInput.value
+                    };
+
+                    localStorage.setItem('eventAnswers', JSON.stringify(answers));
+                    alert('Your answers have been saved!');
+
+                    // Clear the input fields
+                    somethingElseInput.value = '';
+                    document.getElementById('goal-en').value = '';
+                  });
+                
+                
+                
+               
+                  document.getElementById('submit-button').addEventListener('click', function(event) {
+                  event.preventDefault(); // Prevent form submission
+                  const selectedEvent = document.getElementById('goal-en').value;
+                  const somethingElse = document.getElementById('something-else-input').value;
+
+                  const answers = {
+                    selectedEvent: selectedEvent,
+                    somethingElse: somethingElse
+                  };
+
+                  localStorage.setItem('eventAnswers', JSON.stringify(answers));
+                  alert('Your answers have been saved!');
+                  });
+                
