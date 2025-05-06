badc4b90ea2a948ac328aa6e26bb0f8e73ec6d05
TOPIAS_TUOMINEN <Topias.tuominen90@gmail.com>
2025-05-06 10:55:14 +0300

    Update MightyDucks project with latest changes

--- MightyDucks/MightyDucks/server.js
+++ MightyDucks/MightyDucks/server.js
@@ -1,26 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import fs from 'fs';
-import cors from 'cors';
-const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
 
-app.use(cors());
-app.use(bodyParser.json());
-
-app.post('/submit', (req, res) => {
-    const formData = req.body;
-
-    // Append to a file or store in DB here
-    fs.appendFile('submissions.json', JSON.stringify(formData) + '\n', (err) => {
-        if (err) {
-            return res.status(500).send('Error saving data');
-        }
-        res.send('Data saved!');
-    });
+const osallistujaSchema = new mongoose.Schema({
+  nimi: String,
+  
+  aktiviteetti: [String],
 });
 
-app.listen(port, () => console.log(`Server running at http://localhost://127.0.0.1:5500/}`));
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     console.log('DOM fully loaded and parsed');
-//
+module.exports = mongoose.model('Osallistuja', osallistujaSchema);
+
